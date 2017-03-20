70e7d3ec91cc0a791f5ecb082d9dc22c7aa968f5
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-20 17:58:05 +0000

    Add top_up method and unit test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,15 @@ describe Oystercard do
 
   describe '#balance' do
     it 'shows the money on the card' do
-      expect(subject.balance).to eq 0  
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'tops up money on the card' do
+      initial_balance = subject.balance
+      top_up_value = 50
+      expect(subject.top_up(top_up_value)).to eq (initial_balance + top_up_value)
     end
   end
 
