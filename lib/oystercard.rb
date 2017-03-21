2e77a673d1f80078b20ed81cbc9a5eb226a2d2d1
Stephanie Crampin <steph.crampin@gmail.com>
2017-03-21 11:27:18 +0000

    reached ex 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,24 +1,48 @@
 class Oystercard
 
-attr_reader :balance
-
-
+attr_reader :balance, :entry_station, :history, :exit_station
 
   def initialize
     @balance = 0
+    @entry_station = nil
+    @exit_station = nil 
+    @history = []
   end
 
   def top_up(amount)
     raise "Maximum limit exceeded: £#{MAX_LIMIT}" if max?(amount)
-    @balance += amount
+    self.balance += amount
+  end
+
+  def touch_in(station)
+    raise "Insufficient balance" if balance < MIN_LIMIT
+    self.entry_station = station
+  end
+
+  def touch_out(station)
+    self.entry_station = nil
+    self.exit_station = station
+    deduct(MIN_LIMIT)
+  end
+
+  def in_journey?
+    entry_station != nil
   end
 
   private
 
   MAX_LIMIT = 90
+  MIN_LIMIT = 1
+
+  attr_writer :balance, :entry_station, :exit_station
 
   def max?(amount)
-   (@balance + amount) > MAX_LIMIT
+   (self.balance + amount) > MAX_LIMIT
   end
+
+  def deduct(amount)
+    self.balance -= amount
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,20 @@ require 'oystercard'
 
 describe Oystercard do
 
+  max_limit = Oystercard::MAX_LIMIT
+  min_limit = Oystercard::MIN_LIMIT
+
+  let(:station) {double(:station)}
+  let(:station2) {double(:station2)}
+
+  it 'is initialised as not in journey' do
+    expect(subject).not_to be_in_journey
+  end
+
+  it 'has an empty history by default' do
+    expect(subject.history).to eq []
+  end
+
   describe '#balance' do
     it 'shows the money on the card' do
       expect(subject.balance).to eq 0
@@ -13,10 +27,51 @@ describe Oystercard do
       amount = 50
       expect{subject.top_up(amount)}.to change{subject.balance}.by amount
     end
-    it "raises error if top_up will bring balance to more than £#{Oystercard::MAX_LIMIT}" do
-      max_limit = Oystercard::MAX_LIMIT
-      expect { subject.top_up(max_limit + 1) }.to raise_error "Maximum limit exceeded: £#{Oystercard::MAX_LIMIT}"
+    it "raises error if top_up will bring balance to more than £#{max_limit}" do
+      expect { subject.top_up(max_limit + 1) }.to raise_error "Maximum limit exceeded: £#{max_limit}"
     end
+  end
+
+
+
+  describe '#touch_in' do
+    it 'updates the journey status' do
+      subject.top_up(min_limit)
+      subject.touch_in(station)
+      expect(subject).to be_in_journey
+    end
+    it 'raises an error if less than minimum balance' do
+      expect{subject.touch_in(station)}.to raise_error "Insufficient balance"
+    end
+    it 'records the entry station' do
+      subject.top_up(min_limit)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
+    end
+
+  end
+
+  describe '#touch_out' do
+    before do
+      subject.top_up(min_limit)
+      subject.touch_in(station)
+    end
+    it 'updates the journey status to not in journey' do
+      subject.touch_out(station2)
+      expect(subject).not_to be_in_journey
+    end
+    it 'deducts the minimum fare of #{min_limit}' do
+      expect {subject.touch_out(station2)}.to change{subject.balance}.by(-min_limit)
+    end
+    it 'resets the entry station to nil' do
+      subject.touch_out(station2)
+      expect(subject.entry_station).to be_nil
+    end
+    it 'records the exit station' do
+      subject.touch_out(station2)
+      expect(subject.exit_station).to eq station2
+    end
+
   end
 
 end
